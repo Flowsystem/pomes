702679b27793bfb8969e767b2a370192813d620d
Gustavo Bastos <me@gustavovinicius.com>
2018-10-09 18:54:17 +0200

    (break) Add missing specs for Message component

--- test/message.spec.js
+++ test/message.spec.js
@@ -15,4 +15,98 @@ describe('Message', () => {
 
     expect(toJson(message)).toMatchSnapshot();
   });
+
+  describe('future', () => {
+    it('skip translation of future singular messages', () => {
+      const translations = {
+        pt: {
+          'Hello': 'Olá',
+        },
+      };
+      const message = mount(
+        <I18nProvider translations={translations} lang="pt" initialLang="en" initialized>
+          <Message id="Hello {name}" values={{ name: 'foo' }} comment="Foo" future />
+        </I18nProvider>,
+      );
+
+      expect(message.text()).toEqual('Hello foo');
+      expect(toJson(message)).toMatchSnapshot();
+    });
+
+    it('skip translation of future plural messages', () => {
+      const translations = {
+        pt: {
+          'You have one message': 'Você tem uma mensagem',
+          'You have {count} messages': 'Você tem {count} mensagens',
+        },
+      };
+      const message = mount(
+        <I18nProvider translations={translations} lang="pt" initialLang="en" initialized>
+          <Message
+            id="You have one message"
+            pluralId="You have {count} messages"
+            pluralCondition="count"
+            values={{ name: 'foo', count: 2 }}
+            comment="Foo"
+            future
+          />
+        </I18nProvider>,
+      );
+
+      expect(message.text()).toEqual('You have 2 messages');
+      expect(toJson(message)).toMatchSnapshot();
+    });
+  });
+
+  describe('plural', () => {
+    it('translate singular form of plural messages', () => {
+      const singularPTTranslation = 'Você tem uma mensagem';
+      const pluralPTTranslation = 'Você tem {count} mensagens'
+      const translations = {
+        pt: {
+          'You have one message': singularPTTranslation,
+          'You have {count} messages': pluralPTTranslation,
+        },
+      };
+      const message = mount(
+        <I18nProvider translations={translations} lang="pt" initialLang="en" initialized>
+          <Message
+            id="You have one message"
+            pluralId="You have {count} messages"
+            pluralCondition="count"
+            values={{ name: 'foo', count: 1 }}
+            comment="Foo"
+          />
+        </I18nProvider>,
+      );
+
+      expect(message.text()).toEqual(singularPTTranslation);
+      expect(toJson(message)).toMatchSnapshot();
+    });
+
+    it('translate plural messages', () => {
+      const singularPTTranslation = 'Você tem uma mensagem';
+      const pluralPTTranslation = 'Você tem {count} mensagens'
+      const translations = {
+        pt: {
+          'You have one message': singularPTTranslation,
+          'You have {count} messages': pluralPTTranslation,
+        },
+      };
+      const message = mount(
+        <I18nProvider translations={translations} lang="pt" initialLang="en" initialized>
+          <Message
+            id="You have one message"
+            pluralId="You have {count} messages"
+            pluralCondition="count"
+            values={{ name: 'foo', count: 2 }}
+            comment="Foo"
+          />
+        </I18nProvider>,
+      );
+
+      expect(message.text()).toEqual('Você tem 2 mensagens');
+      expect(toJson(message)).toMatchSnapshot();
+    });
+  });
 });
